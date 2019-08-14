c71e332be35fd6e346f87f3d4f116c4be8ea99a9
Ahmad Yadegari <yadegariahmad@gmail.com>
2019-08-14 16:22:46 +0430

    add helmet, compression and cors

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 const { connect } = require('mongoose');
+const { corsWhitelist } = require('./util/misc');
+
 const todoRoutes = require('./routes/todo');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(json()); // application/json
-app.use(cors());
+app.use(cors(corsWhitelist()));
+app.use(helmet());
+app.use(compression());
 
 app.use('/todo', todoRoutes);
 app.use('/auth', authRoutes);
@@ -24,7 +30,7 @@ app.use((error, _req, res) =>
 
 connect(
   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ue3bz.mongodb.net/Todo-app?retryWrites=true`,
-  // { useNewUrlParser: true }
+  { useNewUrlParser: true }
 )
   .then(() =>
   {
--- util/misc.js
+++ util/misc.js
@@ -7,3 +7,22 @@ exports.errorHandler = error =>
 
   return error;
 }
+
+exports.corsWhitelist = () =>
+{
+  const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'http://yadegariahmad.github.io'];
+  const corsOptions = {
+    origin: (origin, callback) =>
+    {
+      if (whitelist.indexOf(origin) !== -1)
+      {
+        callback(null, true)
+      } else
+      {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+
+  return corsOptions;
+}
